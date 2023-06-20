f32d3df267d55604bb9c23def91ff755d74f4680
unknown <harshitgupta23042002@gmail.com>
2023-06-20 16:41:58 +0530

    Ready to get response

--- app/page.js
+++ app/page.js
@@ -1,15 +1,54 @@
+"use client";
+
 import Title from '@/components/title';
 import Welcome from '@/components/welcome';
 import TextArea from '@/components/form.jsx';
 import Response from '@/components/response';
 
+// alert message
+import { useState, forwardRef } from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+
 const Home = ()=>{
-    return <div className='h-screen w-screen relative bg-[#f6f7f9]'>
+
+    // alert message
+    const Alert = forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
+    
+    const [open, setOpen] = useState(false);
+        
+        const handleClose = (event, reason) => {
+            if (reason === 'clickaway') {
+              return;
+            }
+            setOpen(false);
+        };
+        const vertical = 'top';
+        const horizontal = 'center';
+    
+    const [alert, setAlert] = useState("This ia a alert message !");
+
+    /*********************************** */
+
+    return<>
+            
+    <Snackbar anchorOrigin={{ vertical, horizontal }} key={vertical + horizontal} open={open} autoHideDuration={5000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="warning" sx={{ width: '100%' }}>
+            {alert}
+        </Alert>
+    </Snackbar>
+    
+    <div className='h-screen w-screen relative bg-[#f6f7f9]'>
         <Title />
         <Welcome />
-        <TextArea />
+        <TextArea open={setOpen} alert={setAlert}/>
         <Response />
     </div>
+
+    </>
 }
 
 export default Home;
--- components/form.jsx
+++ components/form.jsx
@@ -2,24 +2,52 @@
 
 import { useState } from "react";
 
-const Form = ()=>{
+const Form = ({open, alert})=>{
     const [prompt, setPrompt] = useState("");
 
+
+    // fetchRequest
+    const sendData = async (prompt)=>{
+        try{
+            const response = await fetch('http://localhost:3000/api/input', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                prompt: prompt
+            })
+        });
+
+        if(response.ok){
+            alert('Sent Successfully');
+            open(true);
+        }
+        else{
+            alert("Error occured");
+            open(true);
+        }
+        }catch(err){
+            alert(err.message);
+            open(true);
+        }
+    }
+
     return <div className="mt-[60px] w-screen h-auto flex justify-center items-center">
-        
+
         <div className='min-w-[50vw] flex flex-col justify-center'>
-        <textarea className='w-full p-2 h-[50px]
+        <textarea className='w-full p-3 h-[50px]
         resize-none outline-none text-[16px] rounded-md border-[#dc143c] border-[2px]' placeholder = 'Tell me ...'
         value = {prompt}
         onChange = {(event)=>{
             setPrompt(event.target.value);
-            console.log(prompt);
         }}
         />
         
-        <button className='bg-green-500 border-2 m-4 mr-0 px-4 py-2 rounded-md
+        <button id="submit" className='bg-green-500 border-2 m-4 mr-0 px-4 py-2 rounded-md
          self-end' onClick={()=>{
-            
+            open(true);
+            console.log(prompt);
          }} >Submit</button>
         </div>
         
