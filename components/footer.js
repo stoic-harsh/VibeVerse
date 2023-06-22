510d1f12ac616ffbc3dc719d5a218c5791174fdc
stoic-harsh <137406928+stoic-harsh@users.noreply.github.com>
2023-06-22 22:10:02 +0530

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer(){
     return <div className="flex flex-col justify-center items-center text-white bg-black w-screen absolute bottom-0 h-[100px]">
-            <div className="text-[18px] mx-4 text-center my-1">
+            <div className="text-[18px] mx-4 text-center">
             Made with tons of procrastination | Made by `stoic-harsh`
             </div>
 
